bf52e5cdc5b95ab9fc1675b87dc0bd7d38799018
Michael Dales <michael@digitalflapjack.com>
2023-12-19 11:36:24 +0000

    A working day square bob

--- day9/bin/main.ml
+++ day9/bin/main.ml
@@ -5,7 +5,8 @@ module PaletteMap = Map.Make(struct type t = int let compare = compare end)
 type screen = {
   width   : int ;
   height  : int ;
-  palette : color list;
+  palette : color list ;
+  inverse_palette : color PaletteMap.t ;
 }
 
 let _tic80_palette = "000:1a1c2c5d275db13e53ef7d57ffcd75a7f07038b76425717929366f3b5dc941a6f673eff7f4f4f494b0c2566c86333c57"
@@ -39,10 +40,14 @@ let set_palette_color (palette : color list) (index : int) =
   let colour = List.nth palette index in
   set_color colour
 
-let make_inverse_palette (palette : color list) : color PaletteMap.t = 
-  let indexed_palette = List.mapi (fun index col -> (index, col)) in
-  let map : color PaletteMap.t = PaletteMap.empty in
-    List.fold_left (fun index col -> PaletteMap.add col index map) map indexed_palette;
+let make_inverse_palette (palette : color list) : color PaletteMap.t =
+  let indexed_palette = List.mapi (fun index col -> (index, col)) palette in
+  let palette_map : color PaletteMap.t = PaletteMap.empty in
+    List.fold_left (fun map icol ->
+      let index, col = icol in
+        PaletteMap.add col index map
+      ) palette_map indexed_palette
+
 
 (* ----- *)
 
@@ -55,19 +60,29 @@ let tick (t : int) (screen : screen) =
   let buffer = create_image screen.width screen.height in
   blit_image buffer 0 0;
 
-  let ft = float_of_int t in
-  let fx = 120. +. (sin ((2.28 +. (0.2 *. cos (ft /. 100.))) *. ft /. 80.) *. 100.)
-  and fy = 68. +. (cos ((3.7 +. (0.4 *. sin (ft /. 10.))) *. ft /. 80.) *. 60.) in
+  let dot_size = 24 in
+  let ft = float_of_int t
+  and fw = float_of_int (screen.width - dot_size)
+  and fh = float_of_int (screen.height - dot_size) in
+  let fx = (fw /. 2.) +. (sin ((2.28 +. (0.2 *. cos (ft /. 100.))) *. ft /. 80.) *. (fw /. 2.))
+  and fy = (fh /. 2.) +. (cos ((3.7 +. (0.4 *. sin (ft /. 10.))) *. ft /. 80.) *. (fh /. 2.)) in
   let x = int_of_float fx
   and y = int_of_float fy in
- 
+
   let raw = dump_image buffer in
 
-  (* if (t mod 50) == 0 then
-    
 
-   for j = 0 to (screen.height - 1) do
-     for i = 0 to (screen.width - 1) do
+  (* if (t mod 50) == 0 then
+    for j = 0 to (screen.height - 1) do
+      let row = raw.(j) in
+      for i = 0 to (screen.width - 1) do
+        let old_val = row.(x + i) in
+        let old_pal = PaletteMap.find old_val screen.inverse_palette in
+        if old_pal <
+        let new_pal = (old_pal + 1) mod (List.length screen.palette) in
+        row.(x + i) <- List.nth screen.palette new_pal
+
+
        let a = i + j * screen.width in
        v=peek(a,4)
        if v<8 then
@@ -78,15 +93,16 @@ let tick (t : int) (screen : screen) =
    end
    poke(0x3FF8*2,v,4)
  end *)
- 
-  for j = -3 to 3 do
-    let row = raw.(y + j) in 
-	  for i = -3 to 3 do
+
+  for j = 0 to (dot_size - 1) do
+    let row = raw.(y + j) in
+	  for i = 0 to (dot_size - 1) do
         let old_val = row.(x + i) in
-        row.(x + i) <- (old_val + 1) mod 8
+        let old_pal = PaletteMap.find old_val screen.inverse_palette in
+        let new_pal = (old_pal + 1) mod (List.length screen.palette) in
+        row.(x + i) <- List.nth screen.palette new_pal
     done
   done;
-
 
 
   let new_buffer = make_image raw in
@@ -99,10 +115,12 @@ let inner_tick (t : int) (screen : screen) =
   synchronize ()
 
 let () =
+  let palette = load_tic80_palette vapour_palette in
   let screen : screen = {
     width = 640 ;
     height = 480 ;
-    palette = load_tic80_palette vapour_palette ;
+    palette = palette ;
+    inverse_palette = make_inverse_palette palette ;
   } in
 
   open_graph (Printf.sprintf " %dx%d" screen.width screen.height);
